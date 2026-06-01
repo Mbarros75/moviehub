2b1c7610fa4659bab2fd2440372f512d1c10b8eb
Mbarros75 <michael.barros75@gmail.com>
2026-06-01 17:56:37 -0300

    Add Documentario class with duration handling

--- src/Documentario.java
+++ src/Documentario.java
@@ -1,1 +1,22 @@
+public class Documentario extends Titulo {
 
+    private int duracao; // em minutos
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    @Override
+    public int calcularTempo() {
+        return duracao;
+    }
+
+    @Override
+    public String toString() {
+        return "Documentario: " + getNome() + " | Duração: " + duracao + " min";
+    }
+}
